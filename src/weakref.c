e92da203ea09d3c46b4fcbd9cfc54d657c279d06
dearblue <dearblue@users.noreply.github.com>
2019-06-09 13:29:10 +0900

    `mrb_iv_defined()` による確認は不要で、特異クラスかを確認するべき

--- src/weakref.c
+++ src/weakref.c
@@ -171,7 +171,7 @@ delink_weakref(mrb_state *mrb, struct RData *capture)
   capturep->backref = NULL;
   if (MRB_FROZEN_P(mrb_obj_ptr(target))) { return; }
   mrb_value sclass = mrb_singleton_class(mrb, target);
-  if (mrb_iv_defined(mrb, sclass, id_backref)) {
+  if (mrb_type(sclass) == MRB_TT_SCLASS) {
     mrb_value backrefs = mrb_iv_get(mrb, sclass, id_backref);
     if (mrb_type(backrefs) == MRB_TT_ARRAY) {
       aux_ary_delete_once(mrb, mrb_ary_ptr(backrefs), mrb_obj_value(capture));
