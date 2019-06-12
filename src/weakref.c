e3a83c8685d46914e7f38d908f3be66ef6eadd10
dearblue <dearblue@users.noreply.github.com>
2019-06-12 23:04:47 +0900

    不要な `mrbx_proc_from_method()` 関数と `mrbx_yield_super()` 関数を mruby-aux 送り

--- src/weakref.c
+++ src/weakref.c
@@ -65,53 +65,6 @@ aux_ary_delete_once(mrb_state *mrb, struct RArray *ary, mrb_value obj)
   }
 }
 
-static mrb_value
-aux_proc_from_method(mrb_state *mrb, mrb_method_t m)
-{
-  struct RProc *proc;
-
-  if (MRB_METHOD_FUNC_P(m)) {
-    proc = mrb_proc_new_cfunc(mrb, MRB_METHOD_FUNC(m));
-  } else {
-    proc = MRB_METHOD_PROC(m);
-  }
-
-  return mrb_obj_value(proc);
-}
-
-static mrb_value
-aux_yield_super(mrb_state *mrb, mrb_value self, mrb_int argc, const mrb_value *argv, struct RClass **c)
-{
-  int ai = mrb_gc_arena_save(mrb);
-  mrb_sym mid = mrb_get_mid(mrb);
-
-  if (mid < 1) {
-nomethod_err:
-    mrb_raise(mrb, E_NOMETHOD_ERROR,
-              "super: no superclass method");
-  }
-
-  struct RClass *dummy_c;
-  if (c == NULL) { dummy_c = NULL; c = &dummy_c; }
-
-  if (*c == NULL) { *c = mrb_class(mrb, self); }
-
-  mrb_method_t m = mrb_method_search_vm(mrb, c, mid);
-  if (MRB_METHOD_UNDEF_P(m) || *c == NULL) { goto nomethod_err; }
-
-  *c = (*c)->super;
-  if (c == NULL) { goto nomethod_err; }
-
-  m = mrb_method_search_vm(mrb, c, mid);
-  if (MRB_METHOD_UNDEF_P(m) || *c == NULL) { goto nomethod_err; }
-
-  mrb_value ret = mrb_yield_with_class(mrb, aux_proc_from_method(mrb, m), argc, argv, self, *c);
-  mrb_gc_arena_restore(mrb, ai);
-  mrb_gc_protect(mrb, ret);
-
-  return ret;
-}
-
 /*
  * class WeakRef
  */
