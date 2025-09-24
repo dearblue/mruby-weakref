b7ba4d388072188c438c4b5e1825f9060360886e
dearblue <dearblue@gmx.com>
2025-09-24 21:59:27 +0900

    test_config.rb を更新
      - mruby-3.4.0 で mruby-print が削除された。
      - YAML を使う代わりに、Ruby のリテラル表記に置き換える。

--- test_config.rb
+++ test_config.rb
@@ -1,30 +1,3 @@
-#!ruby
-
-require "yaml"
-
-config = YAML.load <<'YAML'
-  common:
-    gems:
-    - :core: "mruby-sprintf"
-    - :core: "mruby-print"
-    - :core: "mruby-string-ext"
-    - :core: "mruby-bin-mrbc"
-    - :core: "mruby-bin-mirb"
-    - :core: "mruby-bin-mruby"
-  builds:
-    host:
-      defines: [MRB_NO_BOXING, MRB_INT64]
-    host-int32:
-      defines: [MRB_NO_BOXING, MRB_INT32]
-    host-nan:
-      defines: MRB_NAN_BOXING
-    host++-word:
-      defines: MRB_WORD_BOXING
-      c++abi: true
-YAML
-
-MRuby::Lockfile.disable rescue nil
-
 begin
   require "mruby/source"
 rescue LoadError
@@ -32,29 +5,61 @@ rescue LoadError
   require "mruby/source"
 end
 
-config["builds"].each_pair do |n, c|
-  next if MRuby::Source::MRUBY_RELEASE_NO == 20001 && Array(c.dig("defines")).include?("MRB_NAN_BOXING")
+print_gem = MRuby::Source::MRUBY_RELEASE_NO >= 30400 ? nil : [{ core: "mruby-print" }]
+
+config = {
+  common: {
+    gems: [
+      *print_gem,
+      { core: "mruby-sprintf" },
+      { core: "mruby-string-ext" },
+      { core: "mruby-bin-mrbc" },
+      { core: "mruby-bin-mirb" },
+      { core: "mruby-bin-mruby" }
+    ]
+  },
+  builds: {
+    host: {
+      defines: %w(MRB_NO_BOXING MRB_INT64)
+    },
+    "host-int32": {
+      defines: %w(MRB_NO_BOXING MRB_INT32)
+    },
+    "host-nan": {
+      defines: %w(MRB_NAN_BOXING)
+    },
+    "host++-word": {
+      defines: %w(MRB_WORD_BOXING),
+      "c++abi": true
+    }
+  }
+}
+
+MRuby::Lockfile.disable rescue nil
+
+config[:builds].each_pair do |n, c|
+  next if MRuby::Source::MRUBY_RELEASE_NO == 20001 && Array(c.dig(:defines)).include?("MRB_NAN_BOXING")
 
   MRuby::Build.new(n) do |conf|
     toolchain :clang
 
-    conf.build_dir = File.join("build", c["build_dir"] || name)
+    conf.build_dir = File.join("build", c[:build_dir] || name)
 
     enable_debug
     enable_test
-    enable_cxx_abi if c["c++abi"]
+    enable_cxx_abi if c[:"c++abi"]
 
-    cc.defines << [*c["defines"]]
-    cc.flags << [*c["cflags"]]
+    cc.defines << [*c[:defines]]
+    cc.flags << [*c[:cflags]]
 
-    Array(config.dig("common", "gems")).each { |*g| gem *g }
-    Array(c["gems"]).each { |*g| gem *g }
+    Array(config.dig(:common, :gems)).each { |*g| gem *g }
+    Array(c[:gems]).each { |*g| gem *g }
 
     gem __dir__ do |g|
       if g.cc.command =~ /\b(?:g?cc|clang)\d*\b/
         g.cxx.flags << "-std=c++11"
         g.cxx.flags << %w(-pedantic -Wall -Wextra -Wno-unused-parameter)
-        g.cc.flags << (c["c++abi"] ? "-std=c++11" : "-std=c11")
+        g.cc.flags << (c[:"c++abi"] ? "-std=c++11" : "-std=c11")
         g.cc.flags << %w(-Wpedantic -Wall -Wextra -Wno-unused-parameter)
       end
     end
