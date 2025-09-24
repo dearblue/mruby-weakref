2f35cf167c9ee369d302d37f0565b758a633f0a9
dearblue <dearblue@gmx.com>
2025-09-24 21:15:44 +0900

    テストを修正
    GC されるタイミングによってテストが失敗するため。

--- test/weakref.rb
+++ test/weakref.rb
@@ -6,11 +6,15 @@ assert "WeakRef" do
 
   assert_equal src.inspect, ref.inspect
 
+  GC.start
+  GC.disable
+
   ref = WeakRef.new(src.dup)
 
   assert_equal src.inspect, ref.inspect
 
-  20.times { GC.start }
+  GC.enable
+  GC.start
 
   assert_raise(WeakRef::RefError) { ref.inspect }
 
